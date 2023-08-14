6b9d45c203602882edad5a1770be25750a60336c
Fahrurrozi Arul <fahrurrozi.arul3@gmail.com>
2023-08-15 00:20:32 +0700

    add login and home

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,69 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="/home" class="brand-link">
+        <img src="{{ asset('admin/dist/images/LOGO_BAITUL_MALL.png') }}" alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-2" style="opacity: .8">
+        <span class="brand-text">Baitul Mall Klangenan</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="info">
+                <a class="d-block">{{ Auth::User()->name }}</a>
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+                <li class="nav-item">
+                    <a href="{{ route('home') }}" class="nav-link @yield('dashboard')">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item @yield('data.otdp')">
+                    <a href="#" class="nav-link @yield('nav')">
+                        <i class="nav-icon fas fa-table"></i>
+                        <p>
+                            Data Otdp
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+
+                    <ul class="nav nav-treeview">
+                        {{-- otdp --}}
+                        {{-- <li class="nav-item">
+                            <a href="{{ route('otdp.index') }}" class="nav-link @yield('otdp')">
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-bars nav-icon"></i>
+                                <p>Data Otdp</p>
+                            </a>
+                            @role('admin')
+                                <a href="{{ route('recomendation.index') }}" class="nav-link @yield('recomendation')">
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-bars nav-icon"></i>
+                                    <p>Rekomendasi Bantuan</p>
+                                </a>
+                            @endrole
+                            <a href="{{ route('hasil.index') }}" class="nav-link @yield('hasil')">
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-bars nav-icon"></i>
+                                <p>Laporan</p>
+                            </a>
+                        </li> --}}
+                    </ul>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
---
+++ resources/views/admin/pages/home.blade.php
@@ -0,0 +1,78 @@
+@extends('admin.layouts.app')
+@section('title', 'Dashboard')
+@section('dashboard', 'active')
+@section('content')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5><b>LAYANAN PEMBUATAN PENGANTAR BAGI ORANG TERLANTAR DALAM PERJALANAN (OTDP)</b></h5>
+                            </div>
+                            <div class="card-body">
+                                <p>&nbsp&nbsp&nbspPelayanan Orang Terlantar Dalam Perjalanan (otdp) merupakan layanan yang
+                                    diberikan
+                                    pemerintah daerah kota cirebon melalui dinas sosial kepada masyarakat miskin atau
+                                    tidak
+                                    mampu yang kehabisan ongkos atau bekal sehingga terlantar di wilayah kota cirebon.
+                                    kegiatan ini bertujuan memberikan bantuan kepada masyarakat miskin atau tidak mampu
+                                    yang
+                                    terlantar di wilayah kota cirebon untuk dikembalikan ke daerah asal sebagai bentuk
+                                    kepedulian kepada sesama. tata cara pengembalian “otdp” ini dilakukan bersama dinas
+                                    perhubungan melalui upt. terminal harjamukti yang selanjutnya diberangkatkan ke daerah
+                                    asal baik secara langsung maupun estafet.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5><b>Persyaratan Orang Terlantar Dalam Perjalanan (OTDP)</b></h5>
+                            </div>
+                            <div class="card-body">
+                                <p>
+                                    1. Surat Keterangan Terlantar dari Polsek atau Polres wilayah hukum di Kota Cirebon.
+                                    <br>
+                                    2. Surat Pengantar dari Dinas Sosial sebelumnya (jika otdp tersebut estafet). <br>
+                                    3. Menunjukan KTP asli ataupun fotocopy (Opsional). <br>
+                                    Dari 3 persyaratan diatas ada satu persyatan yang wajib orang terlantar bawa yaitu
+                                    adalah Surat Keterangan Terlantar dari Polsek atau Polres Wilayah Hukum yang ada di Kota
+                                    Cirebon
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
